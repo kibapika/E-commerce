98da5fe9479c4ae08a5d58c1b6e48be7bdab1262
Eva Li <evali3101@gmail.com>
2023-03-08 13:06:34 -0500

    editing navbar

--- client/features/Navbar/Navbar.js
+++ client/features/Navbar/Navbar.js
@@ -1,42 +1,26 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import { logout } from '../../app/store.js';
-import mainlogo from '../../../public/pictures/mainlogo.png';
-// import cart from '../pictures/cart.png';
-import {ShoppingBasket} from '@mui/icons-material'
-import './navbar.css'
-import OrderHx from '../OrderHx/orderHx.js';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../../app/store.js";
+import mainlogo from "../../../public/pictures/mainlogo.png";
+import { ShoppingBasket } from "@mui/icons-material";
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import "./navbar.css";
+import OrderHx from "../OrderHx/orderHx.js";
 
 const Navbar = () => {
+
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const logoutAndRedirectHome = () => {
+
+  const logoutRedirect = () => {
     dispatch(logout());
-    navigate('/login');
+    navigate("/home");
   };
 
   const cart = useSelector((state) => state.cart);
-  // console.log('cart', cart);
-
-  // const isLoggedIn = false;
-
-  // placeholder functions for each button
-  const AthleticBtn = () => {
-    console.log('filter athletic sneakers');
-  };
-
-  const CasualBtn = () => {
-    console.log('filter casual sneakers');
-  };
-
-  const ExploreBtn = () => {
-    console.log('filter all sneakers')
-  }
-  const SearchSneaks = () => {
-    console.log('sneakers searched');
-  };
 
   return (
     <div>
@@ -48,48 +32,34 @@ const Navbar = () => {
             <Link className="navLinks" to="/home">
               Home
             </Link>
-          <Link className='sneakBtns' to="/products">Explore</Link>
-          <Link className='sneakBtns' to="/orderHx">Order History</Link>
-            {/* <button className="sneakBtns" type="button" onClick={AthleticBtn}>
-              Athletic
-            </button>
-            <button className="sneakBtns" type="button" onClick={CasualBtn}>
-              Casual
-            </button> */}
+            <Link className="sneakBtns" to="/products">
+              Explore
+            </Link>
+            <Link className="sneakBtns" to="/orderHx">
+              Order History
+            </Link>
           </li>
           <li className="navbarLi">
-            {/* <input placeholder="search..." type="text" /> */}
-            {/* <input onClick={SearchSneaks} type="submit" /> */}
-            <Link className="navLinks" to="/login">
-              Login
-            </Link>
-            <Link className="navLinks" to="/signup">
-              Sign Up
-            </Link>
-            <Link to="/cart">
-              <ShoppingBasket />
-              {/* <span className='nav_cartCount'>{cart.length}</span> */}
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <div>
-        {isLoggedIn ? (
+            {isLoggedIn ? (
           <div>
             {/* The navbar will show these links after you log in */}
-            <button type="button" onClick={logoutAndRedirectHome}>
+            <button className="navLinks" type="button" onClick={logoutRedirect}>
               Logout
             </button>
           </div>
         ) : (
-          // need to work on home page before log in
           <div>
             {/* The navbar will show these links before you log in */}
-            {/* <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link> */}
+            <Link className="navLinks" to="/login">Login</Link>
+            <Link className="navLinks" to="/signup">Sign Up</Link>
           </div>
         )}
-      </div>
+            <Link id="cart" to="/cart">
+              <ShoppingBagIcon />
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
