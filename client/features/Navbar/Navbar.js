9e0ce722a6660fb445b3c25708f36973f1d4b155
Eva Li <evali3101@gmail.com>
2023-03-08 14:20:37 -0500

    edited css navbar

--- client/features/Navbar/Navbar.js
+++ client/features/Navbar/Navbar.js
@@ -4,12 +4,11 @@ import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../../app/store.js";
 import mainlogo from "../../../public/pictures/mainlogo.png";
 import { ShoppingBasket } from "@mui/icons-material";
-import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 import "./navbar.css";
 import OrderHx from "../OrderHx/orderHx.js";
 
 const Navbar = () => {
-
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
 
   const dispatch = useDispatch();
@@ -24,42 +23,49 @@ const Navbar = () => {
 
   return (
     <div>
-      {/* need to work on routes */}
-      <nav id="navDiv">
-        <ul id="navBar">
-          <li className="navbarLi">
-            <img id="mainlogo" src={mainlogo} alt="mainlogo" />
-            <Link className="navLinks" to="/home">
-              Home
-            </Link>
-            <Link className="sneakBtns" to="/products">
-              Explore
-            </Link>
-            <Link className="sneakBtns" to="/orderHx">
-              Order History
-            </Link>
-          </li>
-          <li className="navbarLi">
-            {isLoggedIn ? (
-          <div>
-            {/* The navbar will show these links after you log in */}
-            <button className="navLinks" type="button" onClick={logoutRedirect}>
-              Logout
-            </button>
-          </div>
-        ) : (
-          <div>
-            {/* The navbar will show these links before you log in */}
-            <Link className="navLinks" to="/login">Login</Link>
-            <Link className="navLinks" to="/signup">Sign Up</Link>
-          </div>
-        )}
-            <Link id="cart" to="/cart">
-              <ShoppingBagIcon />
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <div id="navDiv">
+        <div id="leftNav" >
+          <img id="mainlogo" src={mainlogo} alt="mainlogo" />
+          <Link className="navLinks" to="/home">
+            Home
+          </Link>
+          <Link className="navLinks" to="/products">
+            Explore
+          </Link>
+          <Link className="navLinks" to="/orderHx">
+            Order History
+          </Link>
+        </div>
+{/* ---------------------------------------------------------------- */}
+        <div id="rightNav">
+          {isLoggedIn ? (
+            <div>
+              {/* The navbar will show these links after you log in */}
+              <button
+                className="navLinks"
+                type="button"
+                onClick={logoutRedirect}
+              >
+                Logout
+              </button>
+            </div>
+          ) : (
+            <div>
+              {/* The navbar will show these links before you log in */}
+              <Link className="navLinks" to="/login">
+                Login
+              </Link>
+              <Link className="navLinks" to="/signup">
+                Sign Up
+              </Link>
+            </div>
+          )}
+{/* ---------------------------------------------------------------- */}
+          <Link to="/cart">
+            <ShoppingBagIcon id="cart"/>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
