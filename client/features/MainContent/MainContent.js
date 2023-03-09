034ea8167e56d77303c18452653db6a7ab609e4b
Eva Li <evali3101@gmail.com>
2023-03-09 16:39:58 -0500

    edited main page

--- client/features/MainContent/MainContent.js
+++ client/features/MainContent/MainContent.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import TrendingFlatIcon from "@mui/icons-material/TrendingFlat";
-import EastIcon from '@mui/icons-material/East';
+import EastIcon from "@mui/icons-material/East";
 import "./mainContent.css";
 
 const MainContent = () => {
@@ -14,18 +14,30 @@ const MainContent = () => {
           <Link id="sectionABtn" to="/products">
             <div id="textABtn">
               <span>Shop Now</span>
-              <EastIcon id="aBtnArrow"/>
+              <EastIcon id="aBtnArrow" />
             </div>
           </Link>
         </div>
       </section>
-      <section>
-        <Link to="/products">
-          <button className="filterBtns" type="button">
-            Shoes
-          </button>
+      <section id="sectionB">
+        <Link id="bBtn1" to="/products">
+          Shoes
+          <div id="hide1">Shop Shoes</div>
+        </Link>
+        <Link id="bBtn2" to="/products">
+          Clothing
+          <div id="hide2">Shop Clothing</div>
+        </Link>
+        <Link id="bBtn3" to="/products">
+          Best Sellers
+          <div id="hide3">Shop Best Sellers</div>
+        </Link>
+        <Link id="bBtn4" to="/products">
+          New Arrivals
+          <div id="hide4">Shop New Arrivals</div>
         </Link>
       </section>
+      <section></section>
     </div>
   );
 };
